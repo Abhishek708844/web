6bb358d70e750fa3a3ca588610627bff3308d4a9
Abhishek708844 <168166735+Abhishek708844@users.noreply.github.com>
2024-04-26 14:50:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the same directory as the server file
+
 app.use(express.static(path.join(__dirname)));
 
-// Define a route for serving the HTML file
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
